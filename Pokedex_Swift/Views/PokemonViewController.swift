e59818e895881fe2a03fd8a451a7f6589434be04
Schwickert Bob <schwickertbob@outlook.com>
2023-11-27 10:48:14 +0100

    Not much, wanna finish android

--- Pokedex_Swift/Views/PokemonViewController.swift
+++ Pokedex_Swift/Views/PokemonViewController.swift
@@ -14,16 +14,39 @@ class PokemonViewController: ViewController{
     @IBOutlet var image: UIImageView!
     @IBOutlet var type1: UIImageView!
     @IBOutlet var type2: UIImageView!
+    @IBOutlet var weight: UILabel!
+    @IBOutlet var height: UILabel!
+    @IBOutlet var hp: UILabel!
+    @IBOutlet var attack: UILabel!
+    @IBOutlet var defense: UILabel!
+    @IBOutlet var specialAttack: UILabel!
+    @IBOutlet var specialDefense: UILabel!
+    @IBOutlet var speed: UILabel!
     
     override func viewDidLoad() {
-        print(selectedPokemon!)
-        
         api.fetchPokemon(name: selectedPokemon!.name) { pokemon in
             DispatchQueue.main.async {
-                self.nameLabel.text = pokemon?.name
+                print(pokemon)
+                self.nameLabel.text = pokemon?.name.capitalized
                 if let frontDefaultURL = pokemon?.sprites.front_default {
                     self.loadImageFromURL(url: frontDefaultURL)
                 }
+                
+                print(pokemon?.types[0].type.name.capitalized as Any)
+                self.type1.image = UIImage(named: (pokemon?.types[0].type.name.capitalized)!)
+                if((pokemon?.types.count)! > 1){
+                    self.type2.image = UIImage(named: (pokemon?.types[1].type.name.capitalized)!)
+                }
+                
+                self.weight.text = "\(self.weight.text ?? "") \(pokemon?.weight ?? 0) g"
+                self.height.text = "\(self.height.text ?? "") \(pokemon?.height ?? 0) cm"
+                self.hp.text = "\(self.hp.text ?? "") \(pokemon!.stats[0].base_stat)"
+                self.attack.text = "\(self.attack.text ?? "") \(pokemon!.stats[1].base_stat)"
+                self.defense.text = "\(self.defense.text ?? "") \(pokemon!.stats[2].base_stat)"
+                self.specialAttack.text = "\(self.specialAttack.text ?? "") \(pokemon!.stats[3].base_stat)"
+                self.specialDefense.text = "\(self.specialDefense.text ?? "") \(pokemon!.stats[4].base_stat)"
+                self.speed.text = "\(self.speed.text ?? "") \(String(describing: pokemon?.stats[5].base_stat))"
+
             }
         }
         
